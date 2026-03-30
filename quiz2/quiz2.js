c6d1198c71fcbd480dd1fe920f6eb11fce503a74
kevin <kchen1232@gmail.com>
2026-03-30 19:55:04 -0400

    quiz2

---
+++ quiz2/quiz2.js
@@ -0,0 +1,23 @@
+// Quiz 2
+// Put your javascript here in a document.ready function
+
+alert("The DOM is now loading.");
+
+$(document).ready(function () {
+
+    alert("The DOM is now loaded.");
+    $("title").html('ITWS 1100 - Quiz 2');
+
+    $('.go').click(function () {
+        if ($('title').html() === 'ITWS 1100 - Quiz 2') {    
+        $('title').html('Chen - Quiz 2');}
+        else{
+            $('title').html('ITWS 1100 - Quiz 2');
+        }
+    });
+
+    $('#lastname').hover(function (infunction) {
+        $(this).addClass('makeItPurple');
+    });
+
+});
---
+++ quiz2/readme.md
@@ -0,0 +1,3 @@
+    GitHub ID, repo name, Azure homepage link, and Discord handle 
+    low-bar,   ITWS1100-Chenk17, chenk17rpi.eastus.cloudapp.azure.com ,noiwillnot
+

4c6bb76bd58523f8a3cedc62c6cb9fc5e9ac38ed
kevin <kchen1232@gmail.com>
2026-04-16 16:21:22 -0400

    movie table

--- lab9/inclassexample/movies.php
+++ lab9/inclassexample/movies.php
@@ -45,7 +45,7 @@ if ($havePost) {
    }
 }
 ?>
-
+<h3>Add Movie </h3>
 <form method="post" action="movies.php">
    <label for="title">Title:</label>
    <input type="text" id="title" name="title" required><br>
@@ -55,6 +55,46 @@ if ($havePost) {
 
    <button type="submit" name="save">Save Movie</button>
 </form>
+
+<h3>Movie</h3>
+
+<table id="movieTable">
+   <?php
+   if ($dbOk) {
+
+      $query = 'select * from movie order by year';
+      $result = $db->query($query);
+      $numRecords = $result->num_rows;
+
+      echo '<tr><th>Title:</th><th>Year:</th><th></th></tr>';
+      for ($i = 0; $i < $numRecords; $i++) {
+         $record = $result->fetch_assoc();
+         if ($i % 2 == 0) {
+            echo "\n" . '<tr id="movie-' . $record['movieid'] . '"><td>';
+         } else {
+            echo "\n" . '<tr class="odd" id="movie-' . $record['movieid'] . '"><td>';
+         }
+         echo htmlspecialchars($record['title']);
+         echo '</td><td>';
+         echo htmlspecialchars($record['year']);
+         echo '</td><td>';
+         echo '<img src="resources/delete.png" class="deleteActor" width="16" height="16" alt="delete actor"/>';
+         echo '</td></tr>';
+         // Uncomment the following three lines to see the underlying
+         // associative array for each record.
+         //echo '<tr><td colspan="3" style="white-space: pre;">';
+         //print_r($record);
+         //echo '</td></tr>';
+      }
+
+      $result->free();
+
+      // Finally, let's close the database
+      $db->close();
+   }
+
+   ?>
+</table>
 
 <?php include('includes/foot.inc.php'); 
   // footer info and closing tags

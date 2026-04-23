7ec9561ef235ccd8ce21d00af973c9ad33379aed
kevin <kchen1232@gmail.com>
2026-04-23 17:33:13 -0400

    Update Jquery_ui.js

--- quiz3/Jquery_ui.js
+++ quiz3/Jquery_ui.js
@@ -16,5 +16,16 @@ $(document).ready(function () {
 
     $('#wallForm input[type="text"]').css('background-color', 'yellow');
 
-    $('#wallBody').css('background-color', 'white');
+    $('h3:contains("Add a Comment")').css('text-align', 'center');
+
+    $('h3:contains("Wall")').css({
+        'text-align': 'center',
+        'background-color': 'white'
+    });
+
+    $('#wallBody').css({
+        'background-color': 'white',
+        'width': 'fit-content',
+        'margin': '0 auto'
+    });
 });
